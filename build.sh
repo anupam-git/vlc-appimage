e70cf35fb32543a22c012fa7adf9daff5e4f2d3c
Anupam Basak <anupam.basak27@gmail.com>
2018-02-27 02:54:17 +0530

    Updated to use in nxos docker image

--- build.sh
+++ build.sh
@@ -51,14 +51,14 @@ chmod a+x ./run-patchelf.sh
 wget "https://github.com/azubieta/linuxdeployqt/releases/download/continuous/linuxdeployqt-continuous-x86_64.AppImage"
 # wget "https://github.com/probonopd/linuxdeployqt/releases/download/continuous/linuxdeployqt-continuous-x86_64.AppImage"
 chmod a+x ./linuxdeployqt-continuous-x86_64.AppImage
-LINUX_DEPLOY_QT_EXCLUDE_COPYRIGHTS=true appimage-wrapper ./linuxdeployqt-continuous-x86_64.AppImage vlc-3.0.0/build/org.videolan.vlc.desktop -bundle-non-qt-libs
+LINUX_DEPLOY_QT_EXCLUDE_COPYRIGHTS=true appimage-wrapper linuxdeployqt-continuous-x86_64.AppImage vlc-3.0.0/build/org.videolan.vlc.desktop -bundle-non-qt-libs
 # LINUX_DEPLOY_QT_EXCLUDE_COPYRIGHTS=true ../../linuxdeployqt-continuous-x86_64.AppImage org.videolan.vlc.desktop -appimage
 
 mkdir release
 
 wget https://github.com/AppImage/AppImageKit/releases/download/continuous/appimagetool-x86_64.AppImage
 chmod a+x ./appimagetool-x86_64.AppImage
-appimage-wrapper ./appimagetool-x86_64.AppImage vlc-3.0.0/build/
+appimage-wrapper appimagetool-x86_64.AppImage vlc-3.0.0/build/
 cp ./VLC_media_player*.AppImage release/
 
 md5sum ./VLC_media_player*.AppImage > release/md5.txt
