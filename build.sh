3d04785e73654716ca99f98720aec06c892862eb
Anupam Basak <anupam.basak27@gmail.com>
2018-04-24 14:15:46 +0530

    Use Trusty Image

--- build.sh
+++ build.sh
@@ -2,14 +2,15 @@
 
 VLC_VERSION="vlc-3.0.0"
 
-echo "deb http://in.archive.ubuntu.com/ubuntu/ trusty main" | tee /etc/apt/sources.list.d/trusty.list
+# echo "deb http://in.archive.ubuntu.com/ubuntu/ trusty main" | tee /etc/apt/sources.list.d/trusty.list
 apt-get update
 apt-get --yes install python-software-properties software-properties-common
 add-apt-repository ppa:jonathonf/ffmpeg-3 --yes
 add-apt-repository universe --yes
+add-apt-repositories ppa:beineri/opt-qt-5.10.1-trusty
 apt-get update
 apt-get --yes dist-upgrade
-apt-get --yes install libsystemd-dev libarchive-dev curl build-essential autoconf libtool pkg-config patchelf libtasn1-3-dev libtasn1-3-bin libbsd-dev git bison qtbase5-private-dev libqt5svg5-dev automake autopoint gettext cmake wayland-protocols protobuf-compiler libmpg123-dev libgstreamer-plugins-base1.0-dev libsystemd-dev libarchive-dev libopencv-dev 
+apt-get --yes install libsystemd-dev libarchive-dev cat wt | grep curl build-essential autoconf libtool pkg-config patchelf libtasn1-3-dev libtasn1-3-bin libbsd-dev git bison qt510* automake autopoint gettext cmake wayland-protocols protobuf-compiler libmpg123-dev libgstreamer-plugins-base1.0-dev libsystemd-dev libarchive-dev libopencv-dev 
 
 apt-get build-dep vlc --yes
 
